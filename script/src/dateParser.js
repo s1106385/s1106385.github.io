b725569eca5558f0acd7607e4a1a1a3e5c9dd43c
s1106385 <90541164+s1106385@users.noreply.github.com>
2021-10-02 23:19:16 +0800

    Update holiday

--- script/src/dateParser.js
+++ script/src/dateParser.js
@@ -1,8 +1,8 @@
 var checkZH = /\/zh\/{1}/;
 var holiday = false;
-var holiday = [[1,1],[1,25],[1,27],[1,28],[4,4],[4,10],[4,11],[4,13],[4,30],[5,1],[6,25],[7,1],[10,1],[10,2],[10,26],[12,25],[12,26]];
-var holidaydescription = [["一月一日","農曆年初一","農曆年初三","農曆年初四","清明節","耶穌受難節","耶穌受難節翌日","復活節星期一","佛誕","勞動節","端午節","香港特別行政區成立紀念日","國慶日","中秋節翌日","重陽節翌日","聖誕節","聖誕節後第一個周日"],
-                            ["The first day of January","Lunar New Year's Day","The third day of Lunar New Year","The fourth day of Lunar New Year","Ching Ming Festival","Good Friday","The day following Good Friday","Easter Monday","Birthday of the Buddha","Labour Day","Tuen Ng Festival","Hong Kong Special Administrative Region Establishment Day","National Day","The day following the Chinese Mid-Autumn Festival","The day following Chung Yeung Festival","Christmas Day","The first weekday after Christmas Day"]];
+var holiday = [[1,1],[2,12],[2,13],[2,15],[4,2],[4,3],[4,5],[4,6],[5,1],[5,19],[6,14],[7,1],[9,22],[10,1],[10,14],[12,25],[12,27]];
+var holidaydescription = [["一月一日","農曆年初一","農曆年初二","農曆年初四","耶穌受難節","耶穌受難節翌日","清明節翌日","復活節星期一翌日","勞動節","佛誕","端午節","香港特別行政區成立紀念日","中秋節翌日","國慶日","重陽節","聖誕節","聖誕節後第一個周日"],
+                          ["The first day of January","Lunar New Year's Day","The second day of Lunar New Year","The fourth day of Lunar New Year","Good Friday","The day following Good Friday","The day following Ching Ming Festival","The day following Easter Monday","Labour Day","Birthday of the Buddha","Tuen Ng Festival","Hong Kong Special Administrative Region Establishment Day","The day following the Chinese Mid-Autumn Festival","National Day","Chung Yeung Festival","Christmas Day","The first weekday after Christmas Day"]];
 
 function getHolidayDescription(isEN) {
     month = today.getMonth() + 1;
