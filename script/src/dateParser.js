e248b7f8c5819b1961df986dd79a188b9245bd80
s1106385 <90541164+s1106385@users.noreply.github.com>
2021-11-30 19:17:26 +0800

    Upload Cycle 21121

--- script/src/dateParser.js
+++ script/src/dateParser.js
@@ -1,8 +1,8 @@
 var checkZH = /\/zh\/{1}/;
 var holiday = false;
-var holiday = [[1,1],[2,12],[2,13],[2,15],[4,2],[4,3],[4,5],[4,6],[5,1],[5,19],[6,14],[7,1],[9,22],[10,1],[10,14],[12,25],[12,27]];
-var holidaydescription = [["一月一日","農曆年初一","農曆年初二","農曆年初四","耶穌受難節","耶穌受難節翌日","清明節翌日","復活節星期一翌日","勞動節","佛誕","端午節","香港特別行政區成立紀念日","中秋節翌日","國慶日","重陽節","聖誕節","聖誕節後第一個周日"],
-                          ["The first day of January","Lunar New Year's Day","The second day of Lunar New Year","The fourth day of Lunar New Year","Good Friday","The day following Good Friday","The day following Ching Ming Festival","The day following Easter Monday","Labour Day","Birthday of the Buddha","Tuen Ng Festival","Hong Kong Special Administrative Region Establishment Day","The day following the Chinese Mid-Autumn Festival","National Day","Chung Yeung Festival","Christmas Day","The first weekday after Christmas Day"]];
+var holiday = [[1,1],[2,1],[2,2],[2,3],[4,5],[4,15],[4,16],[4,18],[5,2],[5,9],[6,3],[7,1],[9,12],[10,1],[10,4],[12,26],[12,27]];
+var holidaydescription = [["一月一日","農曆年初一","農曆年初二","農曆年初三","清明節","耶穌受難節","耶穌受難節翌日","復活節星期一","勞動節翌日","佛誕翌日","端午節","香港特別行政區成立紀念日","中秋節後第二日","國慶日","重陽節","聖誕節後第一個周日","聖誕節後第二個周日"],
+                          ["The first day of January","Lunar New Year's Day","The second day of Lunar New Year","The third day of Lunar New Year","Ching Ming Festival","Good Friday","The day following Good Friday","Easter Monday","The day following Labour Day","The day following the Birthday of the Buddha","Tuen Ng Festival","Hong Kong Special Administrative Region Establishment Day","The second day following the Chinese Mid-Autumn Festival","National Day","Chung Yeung Festival","The first weekday after Christmas Day","The second weekday after Christmas Day"]];
 
 function getHolidayDescription(isEN) {
     month = today.getMonth() + 1;
